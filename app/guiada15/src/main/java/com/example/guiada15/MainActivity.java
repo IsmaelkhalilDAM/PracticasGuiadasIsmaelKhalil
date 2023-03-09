6743ab8d420c8d4cb6198b9d1a010656c84d3144
TardeDam <iut15773@educastur.es>
2023-03-09 17:35:21 +0100

    0.222

---
+++ app/guiada15/src/main/java/com/example/guiada15/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.guiada15;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ContentResolver resolver;
+    String[] columnas = {Calls.TYPE, Calls.NUMBER};
+    String[] tipos = {"ENTRANTE", "SALIENTE", "PERDIDA"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resolver = getContentResolver();
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(this::consultar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CALL_LOG}, 1);
+            }
+    }
+    @SuppressLint({"Range", "SetTextI18n"})
+    private void consultar(View view) {
+        textView.setText("");
+        Cursor cursor = resolver.query(Calls.CONTENT_URI, columnas, null, null, null);
+        if (cursor.moveToFirst())
+            do {
+                String tipo = tipos[cursor.getInt(cursor.getColumnIndex(Calls.TYPE)) - 1];
+                String telefono = cursor.getString(cursor.getColumnIndex(Calls.NUMBER));
+                textView.append(String.format("%s : %s\n", tipo, telefono));
+            } while (cursor.moveToNext());
+        else
+            textView.setText("NO HAY LLAMADAS EN EL HISTORIAL");
+        cursor.close();
+    }
+}
---
+++ app/guiada17/src/main/java/com/example/guiada17/ManejadorXML.java
@@ -0,0 +1,46 @@
+package com.example.guiada17;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ManejadorXML extends DefaultHandler {
+    private final StringBuilder resultado = new StringBuilder();
+    private final StringBuilder nodoTexto = new StringBuilder();
+    private boolean entryTag;
+    private String total;
+    public String getResultado() {
+        return resultado.toString();
+    }
+    @Override
+    public void startElement(String uri, String localName, String qName,
+                             Attributes attributes) throws SAXException {
+        nodoTexto.setLength(0);
+        if (localName.equals("entry"))
+            entryTag = true;
+    }
+    @Override
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+        nodoTexto.append(ch, start, length);
+    }
+    @Override
+    public void endElement(String uri, String localName, String qName)
+            throws SAXException {
+        if (localName.equals("totalResults")) {
+            total = nodoTexto.toString();
+        } else if (localName.equals("itemsPerPage")) {
+            resultado.append("Mostrando ");
+            resultado.append(nodoTexto);
+            resultado.append(" resultados de ");
+            resultado.append(total);
+            resultado.append("\n\n");
+        } else if (entryTag && qName.equals("title")) {
+            resultado.append(nodoTexto);
+            resultado.append("\n");
+        } else if (localName.equals("entry")) {
+            entryTag = false;
+            resultado.append("\n");
+        }
+    }
+}
